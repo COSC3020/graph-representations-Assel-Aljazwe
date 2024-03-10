eae43890c0c6ccd85c60cae070c038729ffcbd17
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-03-09 19:12:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
 function convertToAdjList(adjMatrix) {
+    const adjList = adjMatrix.map((row, i) => 
+        row.flatMap((hasEdge, j) => hasEdge ? j : []));
+    return adjList;
+}
+
+function convertToListToMatrix(adjList) {
+    const size = adjList.length;
+    const adjMatrix = Array.from({length: size}, () => Array(size).fill(0));
+    
+    adjList.forEach((edges, i) => {
+        edges.forEach(j => {
+            adjMatrix[i][j] = 1;
+        });
+    });
+    
     return adjMatrix;
 }
